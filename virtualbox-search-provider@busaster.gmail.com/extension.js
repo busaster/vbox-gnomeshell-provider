eb0322efc2a3896981c1580653669a8c9b9b3a2d
Gianrico Busa <busaster@gmail.com>
2014-02-08 17:27:09 +0100

    rewritten with new algorithm and 3.10 compatibility

--- virtualbox-search-provider@busaster.gmail.com/extension.js
+++ virtualbox-search-provider@busaster.gmail.com/extension.js
@@ -1,219 +1,170 @@
-/* Virtualbox Search Provider for Gnome Shell
- *
- * Copyright (c) 2012 Gianrico Busa
- *
- * This programm is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This programm is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/*
+ * VirtualBox search provider for Gnome shell 
+ * Copyright (C) 2013 Gianrico Busa <busaster@gmail.com>
  * 
+ * VirtualBox search provider is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * VirtualBox search provider is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * This plugin is heavily based on Bernd Schlapsi's "Ssh Search Provider for Gnome Shell"
- * Updates for Gnome Shell 3.8 by BowerStudios.com
+ * Thanks to gnekoz for his "VirtualBox machines launcher for Gnome shell" extension from which I took 
+ * the new search engine based on vboxmanage invocation. 
  */
+
 const Main = imports.ui.main;
 const Clutter = imports.gi.Clutter;
-const Gio = imports.gi.Gio;
-const GLib = imports.gi.GLib;
-const Lang = imports.lang;
-const Shell = imports.gi.Shell;
 const St = imports.gi.St;
+const Params = imports.misc.params;
 const Util = imports.misc.util;
+const Lang = imports.lang;
 const IconGrid = imports.ui.iconGrid;
+const GLib = imports.gi.GLib;
 
-// Settings
-const XML_MACHINE_ENTRY = '<MachineEntry .*/>';
+const ExtensionUUID = "virtualbox-search-provider@busaster.gmail.com";
 
-// vboxSearchProvider holds the instance of the search provider
-// implementation. If null, the extension is either uninitialized
-// or has been disabled via disable().
-var vboxSearchProvider = null;
+let provider = null;
 
-const VboxSearchIconBin = new Lang.Class({
-    Name: 'VboxSearchIconBin',
+const VirtualBoxIconBin = new Lang.Class({
+    Name: 'VirtualBoxIconBin',
 
-    _init: function(vbox_app, name) {
+    _init: function(name) {
         this.actor = new St.Bin({ reactive: true,
                                   track_hover: true });
-        this._vbox_app = vbox_app
         this.icon = new IconGrid.BaseIcon(name,
                                           { showLabel: true,
-                                            createIcon: Lang.bind(this, this.createIcon)});
+                                            createIcon: Lang.bind(this, this.createIcon), } );
+
         this.actor.child = this.icon.actor;
         this.actor.label_actor = this.icon.label;
     },
 
-    createIcon: function(size) {
+    createIcon: function (size) {
         let box = new Clutter.Box();
-        let icon = new St.Icon({ icon_name: 'vboxsearch',
+        let icon = new St.Icon({ icon_name: 'virtualbox',
                                  icon_size: size });
         box.add_child(icon);
-        let emblem = new St.Icon({ icon_name: 'computer',
-                                   icon_size: 96});
-        box.add_child(emblem);
         return box;
     }
 });
 
-function VirtualBoxSearchProvider() {
-    this._init();
-}
 
-VirtualBoxSearchProvider.prototype = {
+const VBoxMachinesSearchProvider = new Lang.Class({
+    Name: 'VBoxMachinesSearchProvider',
 
-    _init: function(name) {
-        let filename = '';
-
-		this.id = 'VirtualMachines'
-        this._virtualMachineFiles = [];
-        this._virtualMachineNames = [];
-        
-        // the first thing to do i to look at the Virtuallbox configuration file, VirtualBox.xml, stored in $HOME/.VirtualBox
-        // inside this file there's a section (Tag) "MachineRegistry" where the virtual machines are listed
-        // We build a list of Known virtual machines in that way
-        // then GLib.build_filenamev build a correct filename using a list of string parts
-        filename = GLib.build_filenamev([GLib.get_home_dir(), '/.VirtualBox/', 'VirtualBox.xml']);
-        // with the GIO function file_new_for_path we build a GIO file object with the given filename
-        let configFile = Gio.file_new_for_path(filename);
-        // using GIO method we monitor changing that happens on the configFile. The goal is to mantain a correct list of Vm's
-        this.configMonitor = configFile.monitor_file(Gio.FileMonitorFlags.NONE, null);
-        this.configMonitor.connect('changed', Lang.bind(this, this._onConfigChanged));
-        // when initialized we fire an event of creation in order to fill the VM's list 
-        this._onConfigChanged(null, configFile, null, Gio.FileMonitorEvent.CREATED);
-        // global.log('virtual machine init done ');
-    },
-
-    _onConfigChanged: function(filemonitor, file, other_file, event_type) {
-        if (!file.query_exists (null)) {
-            this._virtualMachines = [];
-            return;
-        }
-
-        if (event_type == Gio.FileMonitorEvent.CREATED ||
-            event_type == Gio.FileMonitorEvent.CHANGED ||
-            event_type == Gio.FileMonitorEvent.CHANGES_DONE_HINT)
-        {
-            // the config file is a xml file in wich we have to find the tag "MachineRegistry"
-            // Inside this tag whe find N "MachineEntry" tags, each one with an attribute, "src", where the VM file full path is stored            
-            let content = String(file.load_contents(null)[1]);
-            this._findMachines(content);
-            this._virtualMachineNames=[];
-            for (var i=0;i<this._virtualMachineFiles.length();i++)
-            {
-              let splittedPath=this._virtualMachineFiles[i].split('/');
-              this._virtualMachineNames[i]=splittedPath[splittedPath.length-1].split('.')[0];
-              // global.log('virtual machine name : '+this._virtualMachineNames[i]);
-            }
-            // global.log('loaded total number of virtual machines : '+this._virtualMachineNames.length);
-        }
+    _init: function (name) {
+        this.id = 'VBoxMachines';
     },
     
-    _findMachines: function(content) {
-        // E4X doesn't like the <?XML ...'> part, removing..
-        content=content.replace(/<\?.*\?>/g,"");
-        let x=new XML(content);
-        let ns=x.namespace();
-        this._virtualMachineFiles=x.ns::Global.ns::MachineRegistry.ns::MachineEntry.@src;
+    createIcon: function (name) {
+        return new VirtualBoxIconBin(name);
     },
-    
-    createResultActor: function(result, terms) {
 
-        let appSys = Shell.AppSystem.get_default();
-        let app = appSys.lookup_app('virtualbox.desktop');
-
-        let icon = new VboxSearchIconBin(app, result.name);
+    createResultActor: function (result, terms) {
+        let icon = this.createIcon(result.name);
         return icon.actor;
     },
 
-    getResultMetas: function(resultIds, callback) {
-        let metas = resultIds.map(this.getResultMeta, this);
+    getResultMeta: function (elem) {
+        return { id: elem.id,
+                 name: elem.name
+               };
+    },
+    
+    _getResultSet: function (results, terms) {
+		var vms;
+		
+		try {
+			vms = String(GLib.spawn_command_line_sync('vboxmanage list vms')[1]);
+		} catch (err) {	
+		   Main.notifyError("VirtualBox machines launcher : " + err.message);		
+			return;
+		}
+		var mainRegExp = new RegExp('\"(.*' + terms + '.*)"\ *\{.*\}','mi');
+		var singleRegExp = new RegExp('\{.*\}','mi');
+		var matches = null;
+		var results = new Array();
+		//log('vms>'+vms);
+		// multiple matches are not handled by RegEx so I remove the matched value from the orignal string and 
+      // loop until mathse is not null		
+		do {
+		 matches = mainRegExp.exec(vms);		 
+       if (matches!=null) {
+         //log('partialM> '+matches[0]); 		          
+		   vms=vms.substring(vms.indexOf(matches[0])+matches[0].length);
+		   var vmid=singleRegExp.exec(matches[0]);
+		   results.push({ if:String(vmid[1]), name:String(matches[1]) });
+		 }   
+		}		
+		while (matches!=null);
+		//log('finalM> '+results);
+		if (results.length==0)
+		  return;
+		this.searchSystem.setResults(this, results);
+    },
+
+    getInitialResultSet: function (terms) {
+        // GNOME 3.4 needs the results returned directly whereas 3.5.1
+        // etc will ignore this and instead need pushResults() from
+        // _getResultSet() above
+        return this._getResultSet(null, terms);
+    },
+
+    getSubsearchResultSet: function (results, terms) {
+        // GNOME 3.4 needs the results returned directly whereas 3.5.1
+        // etc will ignore this and instead need pushResults() from
+        // _getResultSet() above
+        return this._getResultSet(results, terms);
+    },
+          
+    createResultObject: function (metaInfo, terms) {
+       let icona=this.createIcon(metaInfo.name);
+       log('actor : '+icona.actor);
+       return { actor: icona.actor, icon: icona };
+    },
+    
+    filterResults: function (providerResults, maxResults) {
+       return providerResults;
+    },    
+   
+    getResultMetas: function (ids, callback) {
+        let metas = ids.map(this.getResultMeta, this);
         callback(metas);
     },
 
-    getResultMeta: function(resultId) {
-        // global.log("getResultMeta for " + resultId + " with name " + resultId.name );
-        
-        return {'id': resultId,
-                 'name': resultId.name
-               };
-    },
+    activateResult: function (id) {
+        Util.spawn([ 'vboxmanage', 'startvm', id ]);
+    },     
+});
 
-    activateResult: function(id) {
-        // global.log("Start vm : "+id.name);
-        Util.spawn(['vboxmanage', 'startvm', id.name]);
-    },
-
-    getInitialResultSet: function(terms) {
-        // global.log("getInitialResultSet called : terms "+terms);
-        let searchResults = [];
-        for (var i=0; i<this._virtualMachineNames.length; i++) {
-            let searchTarget=this._virtualMachineNames[i]+" (VirtualBox VM)";
-            for (var j=0; j<terms.length; j++) {
-                // global.log("Check if "+searchTarget.toUpperCase()+" matches with "+terms[j].toUpperCase());
-                try {
-                    if (searchTarget.toUpperCase().match(terms[j].toUpperCase())) {
-                        searchResults.push({
-                            'name': this._virtualMachineNames[i],
-                            'file': this._virtualMachineFiles[i]
-                        });
-                    }
-                }
-                catch(ex) {
-                    continue;
-                }
-            }
-        }
-        if (searchResults.length > 0) {
-            // GNOME 3.5.1 or so introduced passing result asynchronously
-            // via pushResults() so try that first - if it fails then
-            // simply return the results to stay compatible with 3.4
-            try {
-                this.searchSystem.pushResults(this, searchResults);
-                // global.log("pushed results : "+searchResults);
-            } finally {
-                return searchResults;
-            }
-        }
-        else {
-            // global.log("NO results for "+term);
-            return searchResults;
-        }
-    },
-
-    getSubsearchResultSet: function(previousResults, terms) {
-        return this.getInitialResultSet(terms);
-    },
-};
-
-function init(meta) {
+function init (meta) {
 }
 
-function enable() {
-    if (vboxSearchProvider==null) {
-		// global.log('Activating vboxSearchProvider')
-        vboxSearchProvider = new VirtualBoxSearchProvider();
-        Main.overview.addSearchProvider(vboxSearchProvider);
+function enable () {	
+    if (!provider) {
+		// Dumb check if VBoxManage exists. If not I'll throw an 
+		// exception to disable the extension
+		try {
+			Util.trySpawn(['vboxmanage']);
+		} catch (err) {
+			Main.notifyError("VirtualBox machines launcher : " + err.message);
+			throw err;
+		}
+      provider = new VBoxMachinesSearchProvider();
+      Main.overview.addSearchProvider(provider);
     }
-    // else
-    //  global.log('vboxSearchProvider NOT NULL and enabling : ERROR ?')
 }
 
 function disable() {
-    if  (vboxSearchProvider!=null) {
-	//global.log('Disabling vboxSearchProvider')
-        Main.overview.removeSearchProvider(vboxSearchProvider);
-        vboxSearchProvider.configMonitor.cancel();
-        vboxSearchProvider = null;
+    if (provider) {
+        Main.overview.removeSearchProvider(provider);
+        provider = null;
     }
-    // else
-    //   global.log('vboxSearchProvider NULL and disabling : ERROR ?')
 }
-
